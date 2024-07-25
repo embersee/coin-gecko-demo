c70459fc02ccbc07e99a9969404e6fae1eb4524d
embersee <philipp@shiyanov.com>
2024-07-25 12:56:44 +0300

    fix: searchbar lag

--- components/ui/search-bar.tsx
+++ components/ui/search-bar.tsx
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/jj94L9h2hHE
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import { Input } from "@/components/ui/input";
 
 export default function SearchBar() {
